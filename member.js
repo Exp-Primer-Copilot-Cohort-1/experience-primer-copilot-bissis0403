0bfdd9fccbfa8bab6f2c1b8cdb894bc8c202fd99
bissis0403 <107165229+bissis0403@users.noreply.github.com>
2023-09-26 14:48:23 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,7 @@
 function memberSkill() {
-    
+    var skill = document.getElementById("skill").value;
+    var skillList = document.getElementById("skillList");
+    var skillOption = document.createElement("option");
+    skillOption.text = skill;
+    skillList.add(skillOption);
 }
